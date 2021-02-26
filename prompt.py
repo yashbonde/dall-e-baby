92f53be31ece8450d7ec31bfcf69e93ad536e7dc
yashbonde <bonde.yash97@gmail.com>
2021-02-26 20:17:27 +0530

    fix prompt bugs

--- prompt.py
+++ prompt.py
@@ -10,19 +10,22 @@ from argparse import ArgumentParser
 
 from dalle import DallETransformer, Dalle, Vqvae, set_seed, Tokenizer
 
-md5 = lambda x: hashlib.md5(x.encode("utf-8")).hexdigest()
+
+def md5(x): return hashlib.md5(x.encode("utf-8")).hexdigest()
 
 def toImage(x):
-  print(x.size())
-  return (x.numpy() * 255).astype(np.uint8)[0].transpose((1, 2, 0))
+  return Image.fromarray((x.numpy() * 255).astype(np.uint8))
+
 
 if __name__ == "__main__":
   args = ArgumentParser(description="Generate image using dall-e-baby")
-  args.add_argument("--dalle-path", type = str, help = "path to dalle weights")
-  args.add_argument("--vqvae-path", type = str, help = "path to VQVAE weights")
-  args.add_argument("--tokenizer", type = str, help = "path to tokenizer")
-  args.add_argument("--seed", type = int, default=4, help = "Seed value for generation")
-  args.add_argument("--folder", type = str, default="./generated", help = "folder to store generated images in")
+  args.add_argument("--dalle-path", type=str, help="path to dalle weights")
+  args.add_argument("--vqvae-path", type=str, help="path to VQVAE weights")
+  args.add_argument("--tokenizer", type=str, help="path to tokenizer")
+  args.add_argument("--seed", type=int, default=4,
+                    help="Seed value for generation")
+  args.add_argument("--folder", type=str, default="./generated",
+                    help="folder to store generated images in")
   args = args.parse_args()
 
   folder_path = os.path.expanduser(args.folder)
@@ -48,9 +51,9 @@ if __name__ == "__main__":
 
   # define config and load the model
   dalle_args = Dalle.parse_name(
-    model_path = args.dalle_path,
-    image_vocab_size=vqvae.num_embeddings,
-    text_vocab_size=tok.get_vocab_size(),
+      model_path=args.dalle_path,
+      image_vocab_size=vqvae.num_embeddings,
+      text_vocab_size=tok.get_vocab_size(),
   )
   dalle = DallETransformer(vqvae.get_model(), dalle_args)
   map_location = "cpu" if not torch.cuda.is_available() else "cuda"
@@ -72,17 +75,19 @@ if __name__ == "__main__":
       break
 
     cap = input_prompt.lower() * 100
-    text_tokens = tok.encode(cap).ids[:dalle_args.text_context_len - 1] + [text_end_id]
-    text_tokens = torch.Tensor(text_tokens).unsqueeze(0).long().to(map_location)
+    text_tokens = tok.encode(
+        cap).ids[:dalle_args.text_context_len - 1] + [text_end_id]
+    text_tokens = torch.Tensor(text_tokens).unsqueeze(
+        0).long().to(map_location)
 
     print("Starting Generation (Scratch)", "-"*70)
     output_images, scores = dalle.complete_image(
-      text_tokens=text_tokens,
-      num_return_sequences=20,
-      top_k = 5,
-      top_p = 0.95,
-      temperature=0.95,
-      _verbose = False
+        text_tokens=text_tokens,
+        num_return_sequences=2,
+        top_k=100,
+        top_p=0.95,
+        temperature=0.99,
+        _verbose=False
     )
 
     # define the unique hash, iterate in case such a prompt already exists
@@ -95,12 +100,12 @@ if __name__ == "__main__":
 
     # save the images
     this_image_paths = []
-    for idx, (o,s) in enumerate(zip(output_images, scores)):
+    for idx, (o, s) in enumerate(zip(output_images, scores)):
       path = os.path.join(folder_path, f"{_hash}_{idx}_{s:.4f}.jpg")
       img = toImage(o.cpu())
       img.save(path)
       this_image_paths.append(path)
     meta_cache[_hash] = {
-      "paths": this_image_paths,
-      "prompt": input_prompt
-    } # update cache for storing information
+        "paths": this_image_paths,
+        "prompt": input_prompt
+    }  # update cache for storing information
